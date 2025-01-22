a567f98f14ec9f374c5c059defeefbd5a9707dac
charlie-slime-sys <sawyer.pulsipher24@student.cart.org>
2025-01-22 14:34:19 -0800

    Finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,5 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-let myVariable = "Bob";
-myVariable = "Steve";
-/*
-// also makes comments, use /* for line-break comments :D
-*/
-let icecream = "chocolate";
-if (icecream === "chocolate") {
-    alert("Yay, I love chocolate icecream!");
-} else {
-    alert("Awww, but chocolate is my favorite... 3:")
-}
 
-alert("Hello!")
-function multiply(num1, num2) {
-    let result = num1 * num2;
-    return result;
-}
-multiply(4, 7);
-multiply(20, 20);
-multiply(0.5, 3);
-document.querySelector("html").addEventListener("click", () => {
-    alert("Ouch! Stop poking me! >N<")
-});
+
 const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
@@ -33,3 +10,24 @@ myImage.addEventListener("click", () => {
         myImage.setAttribute("src", "images/firefox-icon.png");
     }
 });
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Hey there, ${myName}!`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hey there, ${storedName}!`;
+}
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
+
