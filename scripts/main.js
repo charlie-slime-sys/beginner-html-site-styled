a6235e5602b50a0305e3cbf2027c8628f14ec74e
charlie-slime-sys <sawyer.pulsipher24@student.cart.org>
2025-01-22 14:15:51 -0800

    change of icon, capped image size, further cont lesson

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+let myVariable = "Bob";
+myVariable = "Steve";
+/*
+// also makes comments, use /* for line-break comments :D
+*/
+let icecream = "chocolate";
+if (icecream === "chocolate") {
+    alert("Yay, I love chocolate icecream!");
+} else {
+    alert("Awww, but chocolate is my favorite... 3:")
+}
+
+alert("Hello!")
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me! >N<")
+});
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.png")
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+});
